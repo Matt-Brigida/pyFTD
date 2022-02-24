427590c4b7d7b226c2319816325aacbdff2b80e6
Matt Brigida <mdbrigida@gmail.com>
2022-02-23 23:15:21 -0500

    can pull data

--- src/pyFTD/main.py
+++ src/pyFTD/main.py
@@ -1,3 +1,10 @@
+import pandas as pd
+
 base_url = "https://www.sec.gov/files/data/fails-deliver-data/cnsfails"
 
 
+
+
+url = "https://www.sec.gov/files/data/fails-deliver-data/cnsfails202201b.zip"
+
+ftds = pd.read_csv(url, compression='zip', delimiter='|')
